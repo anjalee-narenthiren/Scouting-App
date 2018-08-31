a560cba6b7bbe150e15b7aa181ba95b84f69768d
anjaleenaren <31371146+anjaleenaren@users.noreply.github.com>
2018-08-31 03:34:37 -0400

    Added submit button to scout page

---
+++ Scouting/app/src/main/java/com/example/android/scouting/MenuActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.scouting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button scout_bttn = findViewById(R.id.scout_bttn);
+        scout_bttn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    Log.v("MenuActivity", "Btn Pressed - Enter ScoutActivity");
+                    Intent openScoutActivity = new Intent(MenuActivity.this, ScoutActivity.class);
+                    startActivity(openScoutActivity);
+                }
+            }
+        );
+    }
+
+
+
+
+}
